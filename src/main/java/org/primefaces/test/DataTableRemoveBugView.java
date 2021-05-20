2718b072757a5f812dfd4367323957d9b31f202f
Matthias Pischka <git@pischka.de>
2021-05-20 08:50:37 +0200

    Added Data Table Remove Bug

---
+++ src/main/java/org/primefaces/test/DataTableRemoveBugView.java
@@ -0,0 +1,57 @@
+package org.primefaces.test;
+
+import org.primefaces.test.model.Product;
+
+import javax.annotation.PostConstruct;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Named
+@ViewScoped
+public class DataTableRemoveBugView implements Serializable {
+
+  private List<Product> productList;
+  private Product selectedProduct;
+
+  @PostConstruct
+  public void init() {
+    Product productCar = new Product();
+    productCar.setId("xy123");
+    productCar.setLabel("Car");
+    productCar.setPrice(12399);
+    productCar.setInStock(false);
+
+    Product productBike = new Product();
+    productBike.setId("xz345");
+    productBike.setLabel("Bike");
+    productBike.setPrice(899);
+    productBike.setInStock(true);
+
+    Product productTV = new Product();
+    productTV.setId("yz678");
+    productTV.setLabel("TV");
+    productTV.setPrice(549);
+    productTV.setInStock(true);
+
+    this.productList = new ArrayList<>();
+    this.productList.add(productCar);
+    this.productList.add(productBike);
+    this.productList.add(productTV);
+  }
+
+  public void remove(Product product) {
+    System.out.println("Product to remove: " + product.getLabel());
+    this.productList.remove(product);
+  }
+
+  public List<Product> getProductList() {
+    return productList;
+  }
+
+  public void setProductList(List<Product> productList) {
+    this.productList = productList;
+  }
+}
---
+++ src/main/java/org/primefaces/test/model/Product.java
@@ -0,0 +1,56 @@
+package org.primefaces.test.model;
+
+import java.util.Objects;
+
+public class Product {
+
+  private String id;
+  private String label;
+  private double price;
+  private boolean inStock;
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getLabel() {
+    return label;
+  }
+
+  public void setLabel(String label) {
+    this.label = label;
+  }
+
+  public double getPrice() {
+    return price;
+  }
+
+  public void setPrice(double price) {
+    this.price = price;
+  }
+
+  public boolean isInStock() {
+    return inStock;
+  }
+
+  public void setInStock(boolean inStock) {
+    this.inStock = inStock;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Product product = (Product) o;
+    return Objects.equals(id, product.id);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id);
+  }
+}
